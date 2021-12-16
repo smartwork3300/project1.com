79fc051065ade591eb5a17b232c3371018b9a5e0
smartwork3300 <88019236+smartwork3300@users.noreply.github.com>
2021-12-16 15:37:14 +0530

    message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShopComponent } from './components/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:'home',pathMatch:'full'
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'footer',component:FooterComponent
+  },
+  {
+    path:'header',component:HeaderComponent
+  },
+  {
+    path:'cart',component:CartComponent
+  },
+  {
+    path:'shop',component:ShopComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CartComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
